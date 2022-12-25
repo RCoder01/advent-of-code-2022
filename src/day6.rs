1a25249c3cfb32fd3b1e3d7511009be812c7e0fe
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2022-12-24 21:15:37 -0700

    Day 6

---
+++ src/day6.rs
@@ -0,0 +1,48 @@
+use std::{io::{Seek, BufReader, BufRead}, fs::File, collections::VecDeque};
+
+
+crate::main!(6);
+
+
+fn main_0(input: &mut BufReader<File>) -> Result<(), std::io::Error> {
+    let mut line = String::new();
+    input.read_line(&mut line)?;
+    let mut iter = line.as_bytes().iter().enumerate();
+    let mut last = VecDeque::new();
+    for (index, char) in iter {
+        if last.len() < 3 {
+            last.push_back(*char);
+            continue;
+        }
+        last.push_back(*char);
+        // dbg!{&last, char};
+        if !last.iter().any(|c| last.iter().map(|d| (*d == *c) as u8).sum::<u8>() >= 2) {
+            println!("part 1: {}", index + 1);
+            break;
+        }
+        last.pop_front();
+    }
+    Ok(())
+}
+
+
+fn main_1(input: &mut BufReader<File>) -> Result<(), std::io::Error> {
+    let mut line = String::new();
+    input.read_line(&mut line)?;
+    let mut iter = line.as_bytes().iter().enumerate();
+    let mut last = VecDeque::new();
+    for (index, char) in iter {
+        if last.len() < 13 {
+            last.push_back(*char);
+            continue;
+        }
+        last.push_back(*char);
+        // dbg!{&last, char};
+        if !last.iter().any(|c| last.iter().map(|d| (*d == *c) as u8).sum::<u8>() >= 2) {
+            println!("part 1: {}", index + 1);
+            break;
+        }
+        last.pop_front();
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
 
 fn main() -> std::io::Result<()> {
     day1::main()?;
@@ -10,6 +11,7 @@ fn main() -> std::io::Result<()> {
     day3::main()?;
     day4::main()?;
     day5::main()?;
+    day6::main()?;
     Ok(())
 }
 
